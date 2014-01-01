3383ab7d49da21b943d937a6d4196fda09473fa7
Joesus <joesusnick@gmail.com>
2014-01-01 15:46:16 -0700

    Added new blog post, structured jquery page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ CodingNotes::Application.routes.draw do
 
 
   root 'static_pages#home'
-  match '/about',  to: "static_pages#about",  via: 'get'
-  match '/index',  to: "static_pages#index",  via: 'get'
-  match '/style',  to: "static_pages#style",  via: 'get'
-  match '/ruby',   to: "static_pages#ruby",   via: 'get'
-  match '/js',     to: "static_pages#js",     via: 'get'
-  match '/heroku', to: "static_pages#heroku", via: 'get'
+  match '/about',  to: "static_pages#about",    via: 'get'
+  match '/index',  to: "static_pages#index",    via: 'get'
+  match '/jquery',  to: "static_pages#jquery",  via: 'get'
+  match '/ruby',   to: "static_pages#ruby",     via: 'get'
+  match '/js',     to: "static_pages#js",       via: 'get'
+  match '/heroku', to: "static_pages#heroku",   via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
